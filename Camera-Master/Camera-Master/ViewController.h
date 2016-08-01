9a7da12e4927878a1a126d0b239b32b1e4113ee2
Khai <khaibarca123@gmail.com>
2016-08-02 02:36:54 +0700

    create project

---
+++ Camera-Master/Camera-Master/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Camera-Master
+//
+//  Created by Khai on 02/08/2016.
+//  Copyright © 2016 Khai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
